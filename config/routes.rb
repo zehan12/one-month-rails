9fb50629804ecf66aabca149cc1471ac9f062dba
Zehan Khan <zkhan@hootboard.com>
2024-03-27 22:02:51 +0530

    feat: :office: add about page  and update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'page#home'
+  get  'about' => 'page#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
